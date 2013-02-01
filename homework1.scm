c2fc04e2639fd929e7441c6d3da9d5b74e165d90
tomemelko <tom.emelko@gmail.com>
2013-02-01 14:02:31 -0500

    Finished problem 2

--- homework1.scm
+++ homework1.scm
@@ -1,5 +1,14 @@
 (define insert
   (lambda (n l)
     (cond
+      ((null? l) '())
       ((and (> n (car l)) (<= n (car (cdr l))) (cons (car l) (cons n (cdr l)))))
       (else (cons (car l) (insert n (cdr l)))))))
+
+(define removedups
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      ((null? (cdr l)) l)
+      ((eq? (car l) (car (cdr l))) (removedups (cdr l)))
+      (else (cons (car l) (removedups (cdr l)))))))
